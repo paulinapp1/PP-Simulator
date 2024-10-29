fdc4e1f10abca3c989b9f3210923acd068044438
Paulina Grabska <paulinagrabskainf@gmail.com>
2024-10-29 13:56:41 +0100

    Stwory

---
+++ Simulator/Animals.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace Simulator
+{
+
+    public class Animals
+    {
+        public required string Description { get; init; }
+        public uint Size { get; set; } = 3;
+
+
+        public string Info
+        {
+            get { return $"{Description} <{Size}>"; }
+        }
+    }
+
+
+
+}
---
+++ Simulator/Creature.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace Simulator
+{
+    internal class Creature
+    {
+        private string name;
+        public string Name { get { return name; } set { name = value; } }
+        private int level;
+        public int Level { get { return level; } set { level = value; } }
+        public Creature(string name, int level = 1)
+        {
+            Name = name;
+            Level = level;
+
+        }
+        public Creature() { }
+        public void SayHi()
+        {
+            Console.WriteLine($"Hi I'm {Name} and my level is {Level}");
+        }
+        public string Info
+        {
+            get { return $"{Name} [{Level}]"; }
+        }
+
+    }
+}
