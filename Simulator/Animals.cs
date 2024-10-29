187907d4e968d95e9cfbf8ada79a787fea9047ba
Paulina Grabska <paulinagrabskainf@gmail.com>
2024-10-29 14:00:59 +0100

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -5,7 +5,36 @@ namespace Simulator
 
     public class Animals
     {
-        public required string Description { get; init; }
+        private string description = "Unknown";
+        public required string Description
+        {
+            get { return description; }
+            init
+            {
+                var trimmed = value.Trim();
+                if (trimmed.Length < 3)
+                {
+                    trimmed = trimmed.PadRight(3, '#');
+                }
+                else if (trimmed.Length > 15)
+                {
+                    trimmed = trimmed.Substring(0, 15);
+                }
+                if (!char.IsUpper(trimmed[0]))
+                {
+                    trimmed = char.ToUpper(trimmed[0]) + trimmed.Substring(1); 
+                }
+                trimmed = trimmed.Trim();
+                if (trimmed.Length < 3)
+                {
+                    trimmed = trimmed.PadRight(3, '#');
+                }
+                description = trimmed;
+            }
+
+        }
+
+
         public uint Size { get; set; } = 3;
 
 
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -4,10 +4,55 @@ namespace Simulator
 {
     internal class Creature
     {
-        private string name;
-        public string Name { get { return name; } set { name = value; } }
-        private int level;
-        public int Level { get { return level; } set { level = value; } }
+        private string name="Unknown";
+        public string Name
+        {
+            get { return name; }
+            init
+            {
+                var trimmed = value.Trim();
+                if (trimmed.Length < 3)
+                {
+                    trimmed = trimmed.PadRight(3, '#');
+                }
+                else if (trimmed.Length > 25)
+                {
+                    trimmed = trimmed.Substring(0, 25);
+                }
+                if (!char.IsUpper(trimmed[0]))
+                {
+                    trimmed = char.ToUpper(trimmed[0]) + trimmed.Substring(1); 
+                }
+                trimmed = trimmed.Trim();
+                if (trimmed.Length < 3)
+                {
+                    trimmed = trimmed.PadRight(3, '#');
+                }
+
+
+                name = trimmed;
+
+            }
+        }
+
+        private int level=1;
+        public int Level
+        {
+            get { return level; }
+            init
+            {
+                if (value < 1)
+                {
+                    value = 1;
+                }
+                else if (value > 10)
+                {
+                    value = 10;
+                }
+                level = value;
+            }
+        }
+
         public Creature(string name, int level = 1)
         {
             Name = name;
@@ -19,6 +64,14 @@ namespace Simulator
         {
             Console.WriteLine($"Hi I'm {Name} and my level is {Level}");
         }
+        public void Upgrade()
+        {
+            if (Level < 10)
+            {
+                level++;
+            }
+        }
+
         public string Info
         {
             get { return $"{Name} [{Level}]"; }
