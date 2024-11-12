026141b86cc5eba8e67e56274b88a99e6f54365d
Paulina Grabska <paulinagrabskainf@gmail.com>
2024-11-12 11:49:44 +0100

    Mapy

---
+++ Simulator/Maps/SmallSquareMap.cs
@@ -0,0 +1,90 @@
+﻿
+
+namespace Simulator.Maps
+{
+    public class SmallSquareMap : Map
+    {
+        public int Size { get; }
+        private Rectangle bounds;
+
+        public SmallSquareMap(int size)
+        {
+            if(size<5 || size > 20)
+            {
+                throw new ArgumentOutOfRangeException("Nieprawidłowy rozmiar mapy");
+            }
+            Size = size;
+            int x1 = 0, y1 = 0;
+            int x2 = size - 1;
+            int y2 = size - 1;
+            bounds = new Rectangle(x1, y1, x2, y2);
+
+
+        }
+        public override bool Exist(Point p) 
+        {
+            return bounds.Contains(p);
+        }
+
+        public override Point Next(Point p, Direction d)
+        {
+            Point next;
+            if (Exist(p))
+            {
+                switch (d)
+                {
+                    case (Direction.Up):
+                        next=new Point(p.X, p.Y + 1); break;
+                    case (Direction.Down):
+                        next= new Point(p.X, p.Y - 1); break;
+                    case (Direction.Left):
+                       next= new Point(p.X - 1, p.Y); break;
+                    case (Direction.Right):
+                       next= new Point(p.X + 1, p.Y); break;
+                    default:
+                        return p;
+
+                  
+                }
+                if (Exist(next))
+                {
+                    return next;
+                }
+                
+            }
+            return p;
+
+        }
+            
+                
+        
+
+        public override Point NextDiagonal(Point p, Direction d)
+        {
+            Point nextDiagonal;
+            if (Exist(p))
+            {
+                switch (d)
+                {
+                    case (Direction.Up):
+                        nextDiagonal= new Point(p.X + 1, p.Y + 1); break;
+                    case (Direction.Down):
+                        nextDiagonal= new Point(p.X - 1, p.Y - 1); break;
+                    case (Direction.Right):
+                        nextDiagonal= new Point(p.X + 1,p.Y - 1); break;
+                    case (Direction.Left):
+                        nextDiagonal= new Point(p.X,p.Y + 1); break;
+                    default:
+                        return p;
+                }
+                if (Exist(nextDiagonal))
+                {
+                    return nextDiagonal;
+                }
+
+            }
+            return p;
+            
+        }
+    }
+}
--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -1,18 +1,20 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using Simulator.Maps;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Simulator
 {
     internal class Program
     {
+        
         static void Lab5a()
         {
-            Rectangle test1=new Rectangle(2, 1, 1, 2);
+            Rectangle test1 = new Rectangle(2, 1, 1, 2);
             Console.WriteLine(test1.ToString());
-            try { Rectangle test2 = new Rectangle(1, 1, 1, 1); } 
-            catch(ArgumentException ex) { Console.WriteLine(ex.Message); }
+            try { Rectangle test2 = new Rectangle(1, 1, 1, 1); }
+            catch (ArgumentException ex) { Console.WriteLine(ex.Message); }
             Rectangle test4 = new Rectangle(2, 3, 3, 2);
             Console.WriteLine(test4.ToString());
-            Rectangle test5 = new Rectangle(1,1,5,5);
+            Rectangle test5 = new Rectangle(1, 1, 5, 5);
             Console.WriteLine(test5.ToString());
             Point testPoint1 = new Point(3, 3);
             Console.WriteLine(test5.Contains(testPoint1));
@@ -25,18 +27,69 @@ namespace Simulator
 
 
         }
-       
+        static void Lab5b()
+        {
+            SmallSquareMap testmap1 = new SmallSquareMap(10);
+            try
+            {
+                SmallSquareMap testmap2 = new SmallSquareMap(21);
+            }
+            catch (ArgumentOutOfRangeException ex) { 
+                Console.WriteLine(ex.Message); 
+            }
+            try
+            {
+                SmallSquareMap testmap3 = new SmallSquareMap(4);
+            }
+            catch (ArgumentOutOfRangeException ex) {
+                Console.WriteLine(ex.Message);
+            }
+            Point testPoint1 = new Point(6, 6);
+            Console.WriteLine(testmap1.Exist(testPoint1));
+            Point testPoint2 = new Point(-1, 5);
+            Console.WriteLine(testmap1.Exist(testPoint2));
+            foreach (Direction direction in Enum.GetValues(typeof(Direction)))
+            {
+                Console.WriteLine($"Next method direction (invalid input): {direction} {testmap1.Next(testPoint2, direction)}");
+            }
+            foreach (Direction direction in Enum.GetValues(typeof(Direction)))
+            {
+                Console.WriteLine($"NextDiagonal method direction (invalid input): {direction} {testmap1.NextDiagonal(testPoint2, direction)}");
+
+            }
+            Point testPoint3 = new Point(5, 5);
+            foreach (Direction direction in Enum.GetValues(typeof(Direction)))
+            {
+                Console.WriteLine($"Next method direction: {direction} {testmap1.Next(testPoint3, direction)}");
+            }
+            Point testPoint4 = new Point(4, 4);
+            foreach (Direction direction in Enum.GetValues(typeof(Direction)))
+            {
+                Console.WriteLine($"NextDiagonal method direction: {direction} {testmap1.NextDiagonal(testPoint4, direction)}");
+            }
+          
+            
+           
+        }
+
+
+
+
+
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Starting Simulator! \n");
-            Point p = new(10, 25);
-            Console.WriteLine(p.Next(Direction.Right));
-            Console.WriteLine(p.NextDiagonal(Direction.Right));
+            //  Console.WriteLine("Starting Simulator! \n");
+            //  Point p = new(10, 25);
+            //  Console.WriteLine(p.Next(Direction.Right));
+             //Console.WriteLine(p.NextDiagonal(Direction.Right));
             Lab5a();
-          
+            Lab5b();
+
 
 
         }
     }
 }
+
+
